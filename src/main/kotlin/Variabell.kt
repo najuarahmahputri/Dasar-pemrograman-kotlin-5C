572d3d610cf18996699eeec06969ed9951a42127
WELCOME RAP & NRP <najuarahmahputri@gmail.com>
2024-09-03 13:09:35 +0700

    Variabell.kt

---
+++ src/main/kotlin/Variabell.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+}
