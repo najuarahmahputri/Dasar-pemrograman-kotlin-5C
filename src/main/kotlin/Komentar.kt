328e9a5e82cc10a11302e34e00d964b7cedcad34
WELCOME RAP & NRP <najuarahmahputri@gmail.com>
2024-09-03 12:30:46 +0700

    Komentar.kt

---
+++ src/main/kotlin/Komentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    //contoh komentar satu baris
+    /*
+    contoh komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
