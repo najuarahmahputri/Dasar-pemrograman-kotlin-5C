4179ab39ad851140daa585b8c42913533ae4a542
WELCOME RAP & NRP <najuarahmahputri@gmail.com>
2024-09-03 14:01:34 +0700

    VarVariabel.kt

---
+++ src/main/kotlin/VarVariabel.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = " + varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua varVariabel = " + varVariabel)
+}
