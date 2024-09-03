194ad85637089a18d4ce659bd51f71a2afee0ee6
WELCOME RAP & NRP <najuarahmahputri@gmail.com>
2024-09-03 14:07:41 +0700

    ValVariabel.kt

--- src/main/kotlin/VarVariabel.kt
+++ src/main/kotlin/VarVariabel.kt
@@ -1,4 +1,4 @@
-fun main() {
+fun main(args: Array<String>) {
     var varVariabel: Int
     varVariabel = 56 //assigning first value to varVariabel
     println("Pemberian nilai pertama pada varVariabel = " + varVariabel)
